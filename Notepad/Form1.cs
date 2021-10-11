a97f37e8b93852a9282972283bce2930d137b668
Богдан Мысник <musnik1@outlook.com>
2021-10-11 13:34:02 +0500

    Желтая форма и кнопка закрыть

--- Notepad/Form1.cs
+++ Notepad/Form1.cs
@@ -16,5 +16,10 @@ namespace Notepad
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
